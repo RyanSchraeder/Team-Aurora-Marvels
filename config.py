81f8037d4629aad73751df2709c2249837dd4f7f
Ryan Schraeder <rschraeder2@gmail.com>
2019-04-03 21:08:57 -0600

    Created config file
    Integrated API Keys and source URLs

---
+++ config.py
@@ -0,0 +1,12 @@
+# Open API Key 
+
+api_keys_dict: ({
+api_key_superhero_api = ""
+api_key_comic_vine = ""
+api_key_marvel = ""
+})
+    
+## SOURCE URLS
+# https://superheroapi.com/
+# https://comicvine.gamespot.com/api/
+# https://developer.marvel.com/
