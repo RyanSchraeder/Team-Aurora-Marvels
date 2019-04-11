2787bce4791f7941ce3a0d3f8b6f6386a5fe36ee
RyanSchraeder <rschraeder2@gmail.com>
2019-04-11 12:42:38 -0600

    Uploaded All Updated Files
    You can now work out of the master branch and organize csv variables, file imports, etc. in your Jupyter Notebooks with the associated directory. Many files were duplicate and outdated.

---
+++ Group-Project-One_UDENDATA/config.py
@@ -0,0 +1,12 @@
+# Open API Key 
+
+api_keys_dict: ({
+api_key_superhero_api = "2447086675315528"
+api_key_omdb = ""
+api_key_marvel = "17584d1c69d30e066654f4c90d3da208"
+})
+    
+## SOURCE URLS
+# https://superheroapi.com/
+# https://comicvine.gamespot.com/api/
+# https://developer.marvel.com/
